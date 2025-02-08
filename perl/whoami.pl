56a14f49cee6709fb8cd4986c5976b734780835c
Kaamkiya <ziam3.1415@gmail.com>
2025-02-08 14:57:01 +0000

    feat: Add command `whoami` in Perl (#5)
    ### What is your pull request for?
    
    * What issue does it fix? #4
    
    * [x] Have you followed the guidelines in our Contributing document?
    * [x] Have you checked to ensure there aren't other open [Pull Requests](../../../pulls) for the same update/change?
    
    <!-- You can erase any parts of this template not applicable to your Pull Request. -->
    
    ### New Feature Submissions:
    
    1. [x] Does your submission pass tests?
    2. [x] Have you lint your code locally prior to submission?
    
    Reviewed-on: https://codeberg.org/polyutils/polyutils/pulls/5
    Co-authored-by: Kaamkiya <ziam3.1415@gmail.com>
    Co-committed-by: Kaamkiya <ziam3.1415@gmail.com>

---
+++ perl/whoami.pl
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+my $VERSION = '0.1.0';
+
+GetOptions( 'help' => \my $help
+          , 'version' => \my $version
+          );
+
+if ($help) {
+    print 'Print the user name associated with the current effective user ID.
+Same as id -un.
+
+      --help        display this help and exit
+      --version     output version information and exit
+';
+    exit 0;
+}
+
+if ($version) {
+    print 'whoami (perl polyutils) ' . $VERSION . "\n";
+    exit 0;
+}
+
+print getlogin() . "\n";
+
